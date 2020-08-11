37067d6507b8a6c095505cda41d44856b51e4554
ken-tarox <ken-taro@hotmail.com>
2020-08-11 16:49:55 +0900

    Improve mod

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get 'users/new'
-=======
->>>>>>> sign-up2
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
