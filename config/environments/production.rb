63ada225407cf8c4bb9337161aa2e1dca8742b16
ken-tarox <ken-taro@hotmail.com>
2020-11-04 06:06:58 +0900

    hero4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,13 +60,13 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "onsen_app_production"
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://onseno-1.herokuapp.com'
+  host = 'https://onseno-2.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
