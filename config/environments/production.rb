e792ceb0d27c0e47f8f4d722b037db33447acf0a
ken-tarox <ken-taro@hotmail.com>
2020-11-04 16:31:38 +0900

    hero11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = {  :host => 'https://onsenos.herokuapp.com' }
+  host = 'https://onsenos.herokuapp.com'
+  config.action_mailer.default_url_options = {host: host }
   config.action_mailer.smtp_settings = {
     address:"smtp.gmail.com",
     domain: 'gmail.com',
